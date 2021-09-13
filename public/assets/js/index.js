17fd1f38342afb88fd5eb6b9aa993265931245ab
Galatia <ptrivedius@aol.com>
2021-09-13 08:23:05 -0400

    pushing current code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -158,6 +158,8 @@ const renderNoteList = async (notes) => {
     noteListItems.push(createLi('No saved Notes', false));
   }
 
+  console.log(jsonNotes);
+
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
     li.dataset.note = JSON.stringify(note);
